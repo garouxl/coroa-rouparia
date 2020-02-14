4ad8419f7b600955bea08de863d9e38a8f87eac3
Leandro Almeida <garou_xl@yahoo.com.br>
2020-02-14 16:03:15 -0300

    removing reudux-logger from prod build

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,9 +5,11 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 import rootReducer from './root-reducer'
 
-const middleWares = [
-  logger
-]
+const middleWares = []
+
+if (process.env.NODE_ENV === 'development') {
+  middleWares.push(logger)
+}
 
 export const store = createStore(
   rootReducer,
